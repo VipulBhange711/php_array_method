89905ab4a04d29ec386e5c4b7098a7f3fb665632
Vipul Bhange <vipulbhange4164@gmail.com>
2024-09-05 18:58:05 +0530

    this is array method

---
+++ array.php
@@ -0,0 +1,72 @@
+<?php
+
+// echo"This is array Important  method ";
+
+// $array1= ['one','two','three','four','five'];
+// $array2 = "salman ajay kartik rohit arjun";
+// echo "<br>";
+
+// print_r($array1);
+
+// Q1:-   how to check the variable are array or not 
+// echo is_array($array2);
+
+
+//Q:-2  Remove any element from Nth position from array 
+// unset($array1[0]);
+// print_r($array1);
+
+
+// Q:-3 How to add any value or elements at last position in an array
+// array_push($array1,"six");
+// print_r($array1);
+
+// Q:-4 How to remove any value or elements at last position in an array
+// array_pop($array1);
+// print_r($array1);
+
+
+// Q:- 5 how to find  key in an array
+// print_r(array_keys($array1));
+// array_keys($array1);
+
+// Q:- 6 how to convert any array to string
+// $data = implode($array1);
+// print_r($data);
+
+// Q:- 7 how to convert any  string to array
+// $data = explode(" ", $array2);
+// print_r($data);
+
+
+// Q:- 8 how to merge two array 
+// $arr1 = ['A','B','C'];
+// $arr2 = ['X','Y','Z'];
+// $array3 = array_merge($arr1,$arr2);
+// print_r($array3);
+
+
+//Q:-9 how to remove duplicate value from array 
+// $arr1 = ['A','B','C','D','A','C'];
+// $arr2 = array_unique($arr1);
+// print_r($arr2);
+
+//Q:-10 how to sort an array in ascending order 
+// $arr1 = [1,2,3,0,7,4,3];
+// sort($arr1);
+// print_r($arr1);
+
+//Q:-11 how to sort an array in desending order 
+// $arr1 = [1,2,3,0,7,4,3];
+// rsort($arr1);
+// print_r($arr1);
+
+
+//Q:-12 how to count the number of element in an array
+// $arr1 = [1,2,3,0,7,4,3];
+// $arr1 = ['A','B','C','D','A','C'];
+// $arr2 = count($arr1);
+// print_r($arr2);
+
+
+?>
